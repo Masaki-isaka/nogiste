8c152999cf5b8ac8c85142602c92bdc41cb5ed1f
Masaki-isaka <ldistanceekdnm.issa09@gmail.com>
2021-01-18 21:01:09 +0900

    fourth commit

--- db/migrate/20201118110648_change_good_default_on_nogipost.rb
+++
@@ -1,9 +0,0 @@
-class ChangeGoodDefaultOnNogipost < ActiveRecord::Migration[6.0]
-  def up
-    change_column :nogiposts, :good, :integer, default:0
-  end
-
-  def down
-    change_column :nogiposts, good, :integer
-  end
-end
