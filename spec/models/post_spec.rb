7302a71ca87c970cb6a2ec0af82d90b75aec4e84
Masaki-isaka <ldistanceekdnm.issa09@gmail.com>
2021-06-20 00:08:46 +0900

    first commit

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe "#create" do
+    before do
+      @nogimasa=FactoryBot.create(:nogimasa)
+    end
+
+    it "画像URLがモデルに格納される" do
+      post=FactoryBot.build(:post)
+      post.valid?
+      expect(post).to be_valid
+    end
+  end
 end
