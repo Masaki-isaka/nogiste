031ce756b750ecc327efcc06cae3e140fc2c58ea
Masaki-isaka <ldistanceekdnm.issa09@gmail.com>
2021-08-01 11:08:48 +0900

    authenticate_user変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    before_action :authenticate_user!, only: [:index, :add, :edit, :show, :destroy, :create]
     layout "account"
 
     def after_sign_in_path_for(resource)
